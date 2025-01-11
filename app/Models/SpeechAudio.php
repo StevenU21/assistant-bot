93d6ed7c843150dae10fd5896afcc143aac78e51
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-10 23:30:59 -0600

    Add SpeechAudio model and migration with fillable attributes and route key

--- app/Models/SpeechAudio.php
+++ app/Models/SpeechAudio.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class SpeechAudio extends Model
 {
-    //
+    protected $table = 'speech_audio';
+    protected $fillable = [
+        'title',
+        'content',
+        'voice',
+        'slug'
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
--- database/migrations/2025_01_11_052358_create_speech_audio_table.php
+++ database/migrations/2025_01_11_052358_create_speech_audio_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('speech_audio', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->string('voice');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
