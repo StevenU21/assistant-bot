628ec0faa436a804055a3675886c34c3e863d693
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-10 23:34:46 -0600

    Implement SpeechAudioController with index method for listing speech audios

--- app/Http/Controllers/SpeechAudioController.php
+++ app/Http/Controllers/SpeechAudioController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\SpeechAudio;
+use App\Services\OpenAIService;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class SpeechAudioController extends Controller
 {
-    //
+    protected $OpenAIService;
+
+    public function __construct(OpenAIService $OpenAIService)
+    {
+        $this->OpenAIService = $OpenAIService;
+    }
+
+    public function index(): Response
+    {
+        $speechAudios = SpeechAudio::latest()->paginate(10);
+
+        $speechAudios->getCollection()->transform(function ($speechAudio) {
+            $speechAudio->audioUrl = asset('storage/' . $speechAudio->audio);
+            return $speechAudio;
+        });
+
+        return Inertia::render('SpeechAudio/Index', compact('speechAudios'));
+    }
 }
