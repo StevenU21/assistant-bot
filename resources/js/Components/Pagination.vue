0dbbcbdc999953d349ff512e2211d0e55fd96529
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-14 00:22:55 -0600

    Refactor Pagination component for improved code readability and layout

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,22 +1,15 @@
 <template>
     <div v-if="links.length > 3" class="flex flex-wrap -mb-1">
         <template v-for="(link, key) in links" :key="key">
-            <span
-                v-if="link.url === null"
-                class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
-                v-html="link.label"
-            >
+            <span v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
+                v-html="link.label">
             </span>
-            <Link
-                v-else
-                :href="link.url"
-                class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-gray-700 focus:border-indigo-500 focus:text-indigo-500"
+            <Link v-else :href="link.url" class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-gray-700 focus:border-indigo-500 focus:text-indigo-500"
                 :class="{
                     'bg-gray-800 text-white': link.active,
                     'text-gray-400': !link.active,
                 }"
-                v-html="link.label"
-            >
+                v-html="link.label">
             </Link>
         </template>
     </div>
